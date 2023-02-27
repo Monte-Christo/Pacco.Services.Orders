87e431cc018d023ba7841165b3c455bffe1be5f8
Edgar Knapp <Edgar.Knapp@simcorp.com>
2023-02-27 21:17:22 +0100

    fix test

--- src/Pacco.Services.Orders.Api/Startup.cs
+++ src/Pacco.Services.Orders.Api/Startup.cs
@@ -25,15 +25,7 @@ namespace Pacco.Services.Orders.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services
-                .AddControllersWithViews(ConfigureMvcOptions) 
-                // Newtonsoft.Json is added for compatibility reasons
-                // The recommended approach is to use System.Text.Json for serialization
-                // Visit the following link for more guidance about moving away from Newtonsoft.Json to System.Text.Json
-                // https://docs.microsoft.com/dotnet/standard/serialization/system-text-json-migrate-from-newtonsoft-how-to
-                .AddNewtonsoftJson(options =>
-                {
-                    options.UseMemberCasing();
-                });
+                .AddControllersWithViews(ConfigureMvcOptions);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
